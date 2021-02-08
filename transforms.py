7039c79c68ed914a20d76d2312734c48d3a882d5
daniel03c1 <daniel03c195@gmail.com>
2021-02-08 12:38:10 +0000

    [update] transforms.py update

--- transforms.py
+++ transforms.py
@@ -1,3 +1,7 @@
+import torch
+import torchvision.transforms.functional as TF
+
+
 # automatically registers operations (use @transforms.register)
 class BagOfOps:
     def __init__(self):
@@ -12,11 +16,17 @@ class BagOfOps:
 
 
 class Operation:
-    def __init__(self, magnitude):
-        self.magnitude
+    def __init__(self, magnitude_range):
+        if isinstance(magnitude_range, (int, float)):
+            magnitude_range = [-magnitude_range, magnitude_range]
+        assert len(magnitude_range) == 2, 'range must be a pair of numbers(float)'
+        self.range = magnitude_range
 
     def __call__(self, image):
         pass
+
+    def sample_magnitude(self):
+        return torch.empty(1).uniform_(*self.range).item()
 
 
 transforms = BagOfOps()
@@ -25,31 +35,37 @@ transforms = BagOfOps()
 @transforms.register
 class ShearX(Operation):
     def __call__(self, image):
-        return image
+        return TF.affine(image, angle=0, translate=(0, 0), scale=1., 
+                         shear=(self.sample_magnitude(), 0))
 
 
 @transforms.register
 class ShearY(Operation):
     def __call__(self, image):
-        return image
+        return TF.affine(image, angle=0, translate=(0, 0), scale=1., 
+                         shear=(0, self.sample_magnitude()))
 
 
 @transforms.register
 class TranslateX(Operation):
     def __call__(self, image):
-        return image
+        return TF.affine(image, angle=0, 
+                         translate=(self.sample_magnitude(), 0), 
+                         scale=1., shear=(0, 0))
 
 
 @transforms.register
 class TranslateY(Operation):
     def __call__(self, image):
-        return image
+        return TF.affine(image, angle=0, 
+                         translate=(0, self.sample_magnitude()), 
+                         scale=1., shear=(0, 0))
 
 
 @transforms.register
 class Rotate(Operation):
     def __call__(self, image):
-        return image
+        return TF.rotate(image, self.sample_magnitude())
 
 
 @transforms.register
@@ -85,7 +101,7 @@ class Posterize(Operation):
 @transforms.register
 class Contrast(Operation):
     def __call__(self, image):
-        return image
+        return TF.adjust_contrast(image, self.sample_magnitude())
 
 
 @transforms.register
@@ -121,4 +137,7 @@ class SamplePairing(Operation):
 if __name__ == '__main__':
     print(transforms.ops)
     print(transforms.n_ops)
+
+    x = torch.zeros((32, 32, 3))
+    x = Rotate(30)(x)
 
