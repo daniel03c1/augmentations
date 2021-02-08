fae65a22edcf9b4f41e6916a30824eac732cc08d
daniel03c1 <daniel03c195@gmail.com>
2021-02-08 15:26:46 +0000

    [update] dataloader updates and added test file

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,32 @@
 import torch
+import torchvision
+import torchvision.transforms.functional as TF
 
+
+class EfficientCIFAR10(torchvision.datasets.CIFAR10):
+    def __init__(self, *args, **kwargs):
+        super(EfficientCIFAR10, self).__init__(*args, **kwargs)
+        self.preprocess_data()
+
+    def preprocess_data(self):
+        h, w, c = self.data[0].shape
+        n_samples = len(self.data)
+        new_data = torch.zeros((n_samples, c, h, w))
+
+        for i in range(n_samples):
+            new_data[i] = TF.to_tensor(self.data[i])
+        
+        del self.data
+        self.data = new_data
+
+    def __getitem__(self, index: int):
+        img, target = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
 
---
+++ dataloader_test.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+import torchvision
+from dataloader import *
+
+
+class DataloaderTest(unittest.TestCase):
+    def test_efficient_cifar10(self):
+        dataset = EfficientCIFAR10('/media/data1/datasets/cifar', 
+                                   train=False)
+
+        # without transform
+        x, y = dataset[0]
+        self.assertIsInstance(x, torch.Tensor)
+        self.assertEqual([*x.size()], [3, 32, 32])
+        self.assertLessEqual(x.max(), 1)
+        self.assertGreaterEqual(x.min(), 0)
+
+        # with transform
+        dataset.transform = torchvision.transforms.Normalize(
+            [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+        new_x, new_y = dataset[0]
+        self.assertIsInstance(new_x, torch.Tensor)
+        self.assertEqual([*new_x.size()], [3, 32, 32])
+        self.assertGreater((new_x - x).abs().sum().item(), 0)
+
+
+if __name__ == '__main__':
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    unittest.main()
+
