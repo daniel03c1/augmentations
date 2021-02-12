44fea8ebe5411b3fd4d4cd92517113eb73c80453
daniel03c1 <daniel03c195@gmail.com>
2021-02-12 16:08:02 +0000

    [update] transforms.py

--- transforms.py
+++ transforms.py
@@ -95,14 +95,38 @@ class Invert(Operation):
         if image.max() > 1 or image.min() < 0:
             raise ValueError('the value of image must lie between 0 and 1')
 
-        return 1 - image
+        if self.sample_magnitude() >= self.bias:
+            return 1 - image
+        return image
 
 
 @transforms.register
 class Equalize(Operation):
     # https://github.com/pytorch/vision/pull/3119/files
     def __call__(self, image):
-        raise NotImplemented()
+        org_size = image.size()
+        image = image.reshape(-1, *org_size[-2:])
+        n_sample = image.size(0)
+
+        image = (image * 255).int() # float to int
+
+        for i in range(n_sample):
+            # scale channel
+            sample = image[i]
+            hist = torch.histc(sample.float(), bins=256, min=0, max=255)
+            nonzero_hist = hist[hist != 0]
+
+            if nonzero_hist.numel() > 0:
+                step = (nonzero_hist.sum() - nonzero_hist[-1]) // 255
+
+                lut = (torch.cumsum(hist, 0) + (step // 2)) // step
+                lut = torch.cat([torch.zeros(1, device=image.device), lut[:-1]])
+                lut = lut.clamp(0, 255)
+                image[i] = lut[sample.long()].int()
+            
+        image = (image / 255.).float() # int to float 
+        image = image.reshape(*org_size)
+        return image
 
 
 @transforms.register
@@ -110,7 +134,7 @@ class Solarize(Operation):
     # https://github.com/pytorch/vision/pull/3112/files
     def __call__(self, image):
         # assume given image is floats and values are between 0 and 1
-        threshold = 1 - self.sample_magnitude().abs()
+        threshold = 1 - abs(self.sample_magnitude())
         mask = (image > threshold).float()
         image = mask * (1-image) + (1-mask) * image
         return image
@@ -119,7 +143,6 @@ class Solarize(Operation):
 @transforms.register
 class Posterize(Operation):
     # https://github.com/pytorch/vision/pull/3108/files
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bits = int(1 + 7*self.magnitude)
@@ -129,7 +152,7 @@ class Posterize(Operation):
             raise ValueError('the value of image must lie between 0 and 1')
 
         image = (image * 255).int()
-        mask = -int(2**(8 - bits))
+        mask = -int(2**(8 - self.bits))
         image = image & mask
         image = (image / 255.).float()
         return image
@@ -155,11 +178,39 @@ class Brightness(Operation):
         return TF.adjust_brightness(image, self.sample_magnitude())
 
 
-'''
 @transforms.register
 class Sharpness(Operation):
+    # https://github.com/pytorch/vision/pull/3114/files
+    bias = 1
+
+    def __call__(self, image):
+        sharpness = self.sample_magnitude()
+        image = sharpness * image + (1-sharpness) * self.blur_image(image)
+        image = image.clamp(0., 1.)
+        return image
+
+    @staticmethod
+    def blur_image(image: torch.Tensor) -> torch.Tensor:
+        n_chan = image.size(-3)
+        kernel = torch.ones((3, 3), device=image.device)
+        kernel[1, 1] = 5.
+        kernel /= kernel.sum()
+        kernel = kernel.expand(n_chan, 1, 3, 3)
+
+        single = image.ndim == 3
+        if single:
+            image = image.unsqueeze(0)
+
+        image = torch.nn.functional.conv2d(
+            image, kernel, padding=1, groups=n_chan)
+
+        if single:
+            image = image.squeeze(0)
+
+        return image
 
 
+'''
 @transforms.register
 class Cutout(Operation):
 
@@ -173,8 +224,11 @@ if __name__ == '__main__':
     print(transforms.ops)
     print(transforms.n_ops)
 
-    x = torch.zeros((32, 32, 3))
+    x = torch.zeros((3, 32, 32))
     x = Rotate(30/180)(x)
     print(Rotate(30/180))
     print(transforms[0](0.1))
+
+    for op in transforms.ops:
+        op(0.5)(x)
 
