a4424315d40a74dc5a61eba1731796b3906b2796
daniel03c1 <daniel03c195@gmail.com>
2021-02-22 15:49:20 +0000

    [temp update]

--- adv_autoaug_trainer.py
+++ adv_autoaug_trainer.py
@@ -14,24 +14,31 @@ class AdvAutoaugTrainer:
                  optimizer, 
                  criterion, 
                  name: str,
-                 rl_agent,
                  bag_of_ops,
+                 rl_agent=None,
+                 M=8,
+                 rl_n_steps=16,
                  device=None):
-        self.model = model
+        if device is None:
+            device = get_default_device()
+        self.device = device
+
+        # main model
+        self.model = model.to(self.device)
         self.optimizer = optimizer
         self.criterion = criterion
         self.name = name
+        self.best_acc = 0
 
         self.rl_agent = rl_agent
+        self.rl_n_steps = rl_n_steps
+
         self.bag_of_ops = bag_of_ops
-
-        if device is None:
-            device = torch.device(
-                "cuda" if torch.cuda.is_available() else "cpu")
-        self.device = device
-
-        self.model = self.model.to(self.device)
-        self.best_acc = 0
+        self.M = M
+        self.random_erasing = torchvision.transforms.RandomErasing(
+            p=0.5, scale=(0.5, 0.5), ratio=(1., 1.))
+        self.normalize = transforms.Normalize(
+            [0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
 
         self.op_visit_count = torch.zeros([self.bag_of_ops.n_ops]*2,
                                           dtype=torch.int)
@@ -41,28 +48,30 @@ class AdvAutoaugTrainer:
             n_epochs=200, scheduler=None):
         chan_axis = 1
         '''
-        M = 8
-        
+        self.activation = {}
         # for intermediate outputs
-        activation = {}
         def get_activation(name):
             def hook(model, input, output):
-                activation[name] = input # output
+                self.activation[name] = input # output
             return hook
-        avg = self.model.fc.register_forward_hook(get_activation('avgpool'))
+        hook = self.model.fc.register_forward_hook(get_activation('avgpool'))
         '''
 
         for epoch in range(n_epochs):
+            # for RL
+            if self.rl_agent:
+                losses = torch.zeros(
+                    (self.M-1,), dtype=torch.float, device=self.device)
+                states = torch.zeros(
+                    (self.M-1, 1), dtype=torch.long, device=self.device)
+                policies = self.rl_agent.act(states)
+                prior_probs = policies
+                policies = [self.decode_policy(policy) for policy in policies]
+                print(self.op_visit_count)
+
             '''
-            losses = torch.zeros(
-                (M-1,), dtype=torch.float, device=self.device)
             op_grads = torch.zeros(
-                (M-1,), dtype=torch.float, device=self.device)
-            states = torch.zeros((M-1, 1), dtype=torch.long, device=self.device)
-            policies = self.rl_agent.act(states)
-            prior_probs = policies
-            policies = [self.decode_policy(policy) for policy in policies]
-            print(self.op_visit_count)
+                (self.M-1,), dtype=torch.float, device=self.device)
             '''
             self.model.train()
 
@@ -74,16 +83,27 @@ class AdvAutoaugTrainer:
             for xs, ys in train_dataloader:
                 xs = xs.to(self.device)
                 ys = ys.to(self.device)
-                batch_size = xs.size(0)
                 
-                '''
-                xs = torch.cat([policy(xs) for policy in policies]+[xs], dim=0)
-                ys = ys.repeat(M)
-                '''
+                # multiply data
+                multiply = 1
+                xs = xs.repeat(multiply, 1, 1, 1)
+                ys = ys.repeat(multiply)
 
-                self.optimizer.zero_grad()
+                # subdivide into M parts
+                batch_size = xs.size(0)
+                if self.rl_agent:
+                    mini_size = batch_size // self.M
+                    xs = torch.cat(
+                        [policy(xs[i*mini_size:(i+1)*mini_size]) 
+                         for i, policy in enumerate(policies)] \
+                        + [xs[-mini_size:]],
+                        dim=0)
+
+                xs = self.random_erasing(xs)
+                xs = self.normalize(xs)
 
                 # training
+                self.optimizer.zero_grad()
                 with torch.set_grad_enabled(True):
                     ys_hat = self.model(xs)
                     loss = self.criterion(ys_hat, ys)
@@ -94,7 +114,7 @@ class AdvAutoaugTrainer:
 
                 '''
                 # gradients & losses for RL
-                inter = activation['avgpool'][0] # .squeeze()
+                inter = self.activation['avgpool'][0] # .squeeze()
                 grads = torch.autograd.grad(
                     self.criterion(self.model.fc(inter), ys).mean(),
                     inter)[0].detach()
@@ -107,9 +127,11 @@ class AdvAutoaugTrainer:
                     op_mag /= base_mag # normalize (magnitude keep rises)
                     op_grads[i] = 0.95*op_grads[i] + 0.05*op_mag
 
-                loss = loss.detach()
-                losses = 0.95*losses + 0.05*loss.reshape(M, -1).mean(1)[:-1]
                 '''
+                if self.rl_agent:
+                    loss = loss.detach()
+                    losses = 0.95 * losses \
+                           + 0.05 * loss.reshape(self.M, -1).mean(1)[:-1]
 
                 # metrics
                 pred_cls = torch.argmax(ys_hat, -1)
@@ -118,38 +140,39 @@ class AdvAutoaugTrainer:
                 count += xs.size(0)
 
             # verbose
+            total_loss = total_loss / count
+            total_acc = total_acc / count
             current_lr = self.optimizer.state_dict()['param_groups'][0]['lr']
             print(f'{epoch}({time.time()-start:.3f})-lr:{current_lr:.6f} '
-                  f'loss: {(total_loss/count).item():.5f} '
-                  f'acc: {(total_acc/count).item():.5f}')
+                  f'loss: {total_loss.item():.5f} acc: {total_acc.item():.5f}')
             if val_dataloader is not None:
                 val_loss, val_acc = self.eval(val_dataloader)
                 print(f'validation | loss: {val_loss.item():.5f}, '
                         f'acc: {val_acc.item():.5f}')
+                if val_acc.item() > self.best_acc:
+                    self.best_acc = val_acc.item()
 
             if scheduler:
                 scheduler.step()
 
-        '''
-            # calculate rewards
-            # rewards = (losses - losses.mean()) / (losses.std() + 1e-8) 
-            # rewards = calculate_rewards(losses, op_grads)
-            base_loss = loss[-batch_size:].mean()
-            rewards = - 2*op_grads + losses / base_loss
+            if self.rl_agent:
+                # calculate rewards
+                rewards = losses
 
-            # normalize rewards for stable training
-            rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-8)
-            rewards = rewards.view(-1, 1, 1) # TODO: remove this
-            self.rl_agent.cache_batch(states, prior_probs, rewards, states)
-            self.rl_agent.learn(n_steps=16)
-            self.rl_agent.save()
+                # normalize rewards for stable training
+                rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-8)
+                rewards = rewards.view(-1, 1, 1) # TODO: remove this
+                self.rl_agent.cache_batch(states, prior_probs, rewards, states)
+                self.rl_agent.learn(n_steps=self.rl_n_steps)
+                self.rl_agent.save()
 
-            print(losses / base_loss - 1)
-            print(op_grads)
-            print(rewards)
+                print('losses: ', losses)
+                # print(op_grads)
+                print('rewards: ', rewards.squeeze())
 
-        avg.remove() # detach hook
-        '''
+        # hook.remove() 
+        self.save()
+        print(f"Best ACC: {self.best_acc:.5f}")
 
     def eval(self, dataloader):
         self.model.eval()
@@ -158,18 +181,21 @@ class AdvAutoaugTrainer:
         total_acc = 0
         count = 0 # count the number of samples
 
-        for xs, ys in dataloader: 
-            xs = xs.to(self.device)
-            ys = ys.to(self.device)
+        with torch.no_grad():
+            for xs, ys in dataloader: 
+                xs = xs.to(self.device)
+                ys = ys.to(self.device)
 
-            with torch.set_grad_enabled(False):
-                ys_hat = self.model(xs)
-                loss = self.criterion(ys_hat, ys).mean()
+                xs = self.normalize(xs)
 
-            pred_cls = torch.argmax(ys_hat, -1)
-            total_loss += loss * xs.size(0)
-            total_acc += torch.sum(pred_cls == ys.data)
-            count += xs.size(0)
+                with torch.set_grad_enabled(False):
+                    ys_hat = self.model(xs)
+                    loss = self.criterion(ys_hat, ys).mean()
+
+                pred_cls = torch.argmax(ys_hat, -1)
+                total_loss += loss * xs.size(0)
+                total_acc += torch.sum(pred_cls == ys.data)
+                count += xs.size(0)
 
         return total_loss/count, total_acc/count
 
@@ -235,17 +261,15 @@ if __name__ == '__main__':
     from dataloader import EfficientCIFAR10
     from models import Controller
     from transforms import transforms as bag_of_ops
-    from wide_resnet import WideResNet
+    from wideresnet import WideResNet
 
     # transforms
     data_transforms = {
         'train': transforms.Compose([
+            # transforms.RandomCrop(32, padding=4), # , padding_mode='reflect'),
             transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(32, padding=4),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'val': transforms.Compose([
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
     }
 
@@ -263,7 +287,7 @@ if __name__ == '__main__':
             num_workers=12)
 
     # model
-    model = WideResNet(28, 10, 0.3, num_classes=10)
+    model = WideResNet(28, 10, 0.3, n_classes=10)
 
     criterion = nn.CrossEntropyLoss(reduction='none')
     optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9,
@@ -271,22 +295,25 @@ if __name__ == '__main__':
                           weight_decay=5e-4)
 
     # RL
+    M = 4
     c = Controller(output_size=bag_of_ops.n_ops)
     c_optimizer = optim.Adam(c.parameters(), lr=0.00035)
-    ppo = PPOAgent(c, name='ppo.pt', batch_size=7, augmentation=aug_policies)
+    ppo = PPOAgent(c, name='ppo.pt', batch_size=M-1, augmentation=aug_policies)
 
     trainer = AdvAutoaugTrainer(model=model,
                                 optimizer=optimizer,
                                 criterion=criterion,
                                 name='test.pt',
-                                rl_agent=ppo,
-                                bag_of_ops=bag_of_ops)
+                                bag_of_ops=bag_of_ops,
+                                rl_n_steps=8, # 16,
+                                M=M, 
+                                rl_agent=ppo)
 
     print(bag_of_ops.ops)
     n_epochs = 200
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
-                                                     milestones=[120,160],
-                                                     gamma=0.1)
+                                                     milestones=[60,120,160],
+                                                     gamma=0.2)
     trainer.fit(dataloaders['train'], dataloaders['val'], 
                 n_epochs=n_epochs,
                 scheduler=scheduler)
