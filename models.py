bcc84c4396f4d76b10d487a275af40f1879a46ca
daniel03c1 <daniel03c195@gmail.com>
2021-02-24 11:36:55 +0000

    [update] added PolicyController and removed ContBrain

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 from torchvision import transforms
@@ -73,81 +74,130 @@ class Controller(nn.Module):
         return (probs * probs.log()).sum(-1)
 
 
-class ContBrain(nn.Module):
-    def __init__(self, bag_of_ops, op_layers=4, n_layers=1, h_dim=64, **kwargs):
-        super(ContBrain, self).__init__(**kwargs)
+class PolicyController(nn.Module):
+    def __init__(self, bag_of_ops, op_layers=4, n_layers=2, h_dim=64, **kwargs):
+        super(PolicyController, self).__init__(**kwargs)
         self.bag_of_ops = bag_of_ops
         self.n_ops = bag_of_ops.n_ops
         self.op_layers = op_layers
         self.h_dim = h_dim
         self.max_ratio = 3
 
-        # modules
-        self.encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(
-                d_model=h_dim, nhead=4, dim_feedforward=h_dim*4),
-            num_layers=n_layers)
-        self.embedding = nn.Embedding(3, h_dim) # [op, mag, dist]
-        self.op_linear = nn.Linear(h_dim, self.n_ops) # for op
-        self.mag_linear = nn.Linear(h_dim, self.n_ops) # for mag
-        self.dist_linear = nn.Linear(h_dim, self.n_ops) # for dist
-
-        # inputs
-        seq_len = bag_of_ops.n_ops + op_layers * 2
+        ''' modules '''
+        # fixed inputs
+        seq_len = 2 + op_layers * 2
         self.input = torch.normal(
-            mean=torch.zeros((1, seq_len, h_dim)),
-            std=torch.ones(1, seq_len, h_dim))
-        self.emb_indices = torch.cat(
-            [torch.zeros((1, self.n_ops)),
+            torch.zeros((1, seq_len, h_dim)), torch.ones(1, seq_len, h_dim))
+        self.type_emb_indices = torch.cat(
+            [torch.zeros((1, 2)),
              torch.ones((1, op_layers)),
              2*torch.ones((1, op_layers))],
             -1).long()
+        self.order_emb_indices = torch.arange(op_layers).repeat(2).reshape(1, -1)
 
-        self.scale = torch.arange(0, 1+1e-8, 1/(self.n_ops-1))
+        # front
+        self.type_emb = nn.Embedding(3, h_dim) # [op, mag, dist]
+        self.order_emb = nn.Embedding(op_layers, h_dim) # [1, 2,...]
+
+        # middle
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=h_dim, nhead=4, dim_feedforward=h_dim*2),
+            num_layers=n_layers)
+
+        # back
+        self.mean_linear = nn.Linear(h_dim, self.n_ops) # for op means
+        self.std_linear = nn.Linear(h_dim, self.n_ops) # for op stds
+        self.mag_linear = nn.Linear(h_dim, self.n_ops) # for mag
+        self.dist_linear = nn.Linear(h_dim, self.n_ops) # for dist
 
     def forward(self, x: torch.Tensor):
-        out = self.input + self.embedding(self.emb_indices) + x
-        out = self.encoder(out)
+        probs = self.input
+        probs += self.type_emb(self.type_emb_indices)
+        probs += torch.cat(
+            [torch.zeros((1, 2, self.h_dim)),
+             self.order_emb(self.order_emb_indices)],
+            axis=1)
 
-        op = self.op_linear(out[:, :self.n_ops]).softmax(-1)
-        mag = self.mag_linear(out[:, self.n_ops:-self.op_layers]).softmax(-1)
-        dist = self.dist_linear(out[:, -self.op_layers:])
+        probs = self.encoder(probs)
+
+        mean = self.mean_linear(probs[:, :1]).sigmoid() # [0, 1]
+        std = self.std_linear(probs[:, 1:2]).sigmoid() / 4 # [0, 0.25]
+        mag = self.mag_linear(probs[:, 2:-self.op_layers]).softmax(-1)
+        dist = self.mag_linear(probs[:, -self.op_layers:])
         dist = torch.sigmoid(dist) + 1/(self.max_ratio-1)
         dist /= dist.sum(-1, keepdims=True)
-        out = torch.cat([op, mag, dist], 1)
 
-        return out
+        action_dist = torch.cat([mean, std, mag, dist], 1)
 
-    def decode_policy(self, policy):
-        assert policy.ndim == 2
-        # map categorical dist to magnitudes in range of [0, 1]
-        magnitudes_per_op = torch.matmul(policy[:self.n_ops], self.scale)
-        magnitudes_per_op = torch.clip(magnitudes_per_op, 0, 1)
-        magnitudes_per_layer = torch.matmul(
-            policy[self.n_ops:-self.op_layers], self.scale)
-        magnitudes_per_layer = torch.clip(magnitudes_per_layer, 0, 1)
+        # act
+        n_samples = x.size(0)
+        max_mags = mean + torch.randn((n_samples, 1, self.n_ops)) * std
+        max_mags = torch.clip(max_mags, 0, 1)
+        mag_per_layer = torch.multinomial(mag[0], n_samples).transpose(1, 0)
+        mag_per_layer = torch.eye(self.n_ops)[mag_per_layer]
 
-        probs_per_layer = policy[-self.op_layers:]
+        actions = torch.cat(
+            [max_mags, mag_per_layer, dist.repeat(n_samples, 1, 1)], 1)
+        action_dist = action_dist.repeat(n_samples, 1, 1)
 
-        return RandomApply(self.bag_of_ops, 
-                           probs_per_layer, 
-                           magnitudes_per_op,
-                           magnitudes_per_layer)
+        return actions, action_dist
+
+    def calculate_log_probs(self, actions, actions_dist):
+        # [n_samples, n_ops]
+        max_mags = actions[:, 0]
+        # [n_samples, op_layers, n_ops]
+        mag_per_layer = actions[:, 1:self.op_layers+1] 
+        # [n_samples, op_layers, n_ops]
+        op_dist = actions[:, -self.op_layers:]
+
+        mean = actions_dist[:, 0] # [n_samples, n_ops]
+        std = actions_dist[:, 1] # [n_samples, n_ops]
+        mag = actions_dist[:, 2:-self.op_layers]
+        dist = actions_dist[:, -self.op_layers:]
+
+        # [n_samples, n_ops]
+        op_log_prob = -((max_mags - mean) ** 2) \
+            / (2 * (std**2)) - std.log() - math.log(math.sqrt(2 * math.pi))
+        # [n_samples, op_layers]
+        mag_log_prob = (mag_per_layer * mag).sum(-1).log()
+        # [n_samples, op_layers * n_ops]
+        dist_log_prob = dist.log().view(dist.size(0), -1)
+        
+        return torch.cat([op_log_prob, mag_log_prob, dist_log_prob], 1)
+
+    def calculate_entropy(self, actions_dist):
+        # TODO: need to exactly calculate entropy
+        std = actions_dist[:, 1] # [n_samples, n_ops]
+        rest = actions_dist[:, 2:] 
+
+        std_ent = 0.5 + 0.5 * math.log(2 * math.pi) + std.log()
+        rest_ent = -(rest*rest.log()).sum(-1) # [n_samples, op_layers*2]
+        
+        return torch.cat([std_ent, rest_ent], 1).mean()
+
+    def decode_policy(self, action):
+        assert action.ndim == 2
+        max_mags = action[0:1] # [1, n_ops]
+        mag_per_layer = action[1:self.op_layers+1] # [op_layers, n_ops]
+        op_dist = action[-self.op_layers:]
+
+        # from categorial to real num
+        mag_per_layer = (mag_per_layer.argmax(-1, keepdims=True)+1) \
+                      / self.n_ops
+        magnitudes = max_mags * mag_per_layer
+
+        return RandomApply(self.bag_of_ops, op_dist, magnitudes)
 
 
 class RandomApply(torch.nn.Module):
     def __init__(self, 
                  bag_of_ops, 
                  probs_per_layer, # [op_layers, n_ops]
-                 magnitudes_per_op, # [n_ops]
-                 magnitudes_per_layer):
+                 magnitudes): # [op_layers, n_ops]
         super().__init__()
         self.probs_per_layer = probs_per_layer
-        self.op_layers = magnitudes_per_layer.size(0)
-
-        magnitudes = magnitudes_per_layer.unsqueeze(1) \
-                   * magnitudes_per_op.unsqueeze(0)
-        magnitudes = torch.clip(magnitudes, 0, 1)
+        self.op_layers = magnitudes.size(0)
 
         self.layers = [
             [bag_of_ops[i](magnitudes[j, i].cpu().item()) 
@@ -155,9 +205,9 @@ class RandomApply(torch.nn.Module):
             for j in range(self.op_layers)]
 
     def forward(self, image):
+        opers = self.probs_per_layer.multinomial(1).squeeze()
         for i in range(self.op_layers):
-            op = self.probs_per_layer[i].multinomial(1)[0]
-            image = self.layers[i][op](image)
+            image = self.layers[i][opers[i]](image)
         return image
 
 
@@ -170,14 +220,20 @@ if __name__ == '__main__':
     c = Controller(bag)
     print(sum([p.numel() for p in c.parameters()]))
 
-    brain = ContBrain(bag, op_layers=4)
-    brain.load_state_dict(torch.load('WR_contbrain_ppo.pt'))
+    brain = PolicyController(bag, op_layers=4)
     print(sum([p.numel() for p in brain.parameters()]))
 
-    x = torch.normal(torch.zeros((4, 1, 64)), torch.ones((4, 1, 64)))
-    out = brain(x)
-    import pdb; pdb.set_trace()
-    policy = brain.decode_policy(out[0])
+    x = torch.zeros(2)
+    action, prob = brain(x)
+    log_prob = brain.calculate_log_probs(action, prob)
+    entropy = brain.calculate_entropy(prob)
+
+    print('action', action)
+    print('prob', prob)
+    print('log_prob', log_prob)
+    print('entropy', entropy)
+
+    policy = brain.decode_policy(action[0])
     y = torch.rand((32, 3, 32, 32))
     for i in range(5):
         print(policy(y).size())
