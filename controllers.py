fb961da8383d51080e45cecf2ed7e1d948aa6989
daniel03c1 <daniel03c195@gmail.com>
2021-03-13 15:39:00 +0000

    Add temperature to SGC
    now division by temperature will be applied before softmax (prob)

--- controllers.py
+++ controllers.py
@@ -11,12 +11,14 @@ class SGC(nn.Module):
                  op_layers=4, 
                  n_layers=2, 
                  h_dim=64, 
+                 temperature=1.,
                  **kwargs):
         super(SGC, self).__init__(**kwargs)
         self.bag_of_ops = bag_of_ops
         self.n_ops = bag_of_ops.n_ops
         self.op_layers = op_layers
         self.h_dim = h_dim
+        self.temperature = temperature
 
         ''' modules '''
         # fixed inputs
@@ -110,7 +112,7 @@ class SGC(nn.Module):
         prob = action[-self.op_layers:]
 
         mag = mag.clip(0, 1)
-        prob = prob.softmax(-1)
+        prob = (prob / self.temperature).softmax(-1)
 
         return RandomApply(self.bag_of_ops, prob, mag, double_mag=True)
 
