3830c589a3edbd7160d043313a4033813e7352fd
daniel03c1 <daniel03c195@gmail.com>
2021-03-15 08:44:35 +0000

    Augmented transforms_test.py

--- transforms_test.py
+++ transforms_test.py
@@ -11,30 +11,49 @@ from transforms import *
 class TransformsTest(unittest.TestCase):
     def setUp(self):
         self.pil_img = PIL.Image.open('test_img.jpeg')
-        self.torch_img = np.array(self.pil_img).transpose(2, 0, 1) # to C, H, W
-        self.torch_img = torch.Tensor(self.torch_img)
+        self.np_img = np.array(self.pil_img).transpose(2, 0, 1) # to C, H, W
+        self.torch_img = torch.Tensor(self.np_img) / 255.
 
     def test_autocontrast(self):
         target = PIL.ImageOps.autocontrast(self.pil_img)
         pred = AutoContrast(1.)(self.torch_img)
-
-        self.assertEqual(np.array(pred/255).transpose(2, 0, 1),
-                         pred.numpy())
+        self.compare(target, pred)
 
     def test_invert(self):
-        raise NotImplemented()
+        target = PIL.ImageOps.invert(self.pil_img)
+        pred = Invert(1.)(self.torch_img)
+        self.compare(target, pred)
 
     def test_equalize(self):
-        raise NotImplemented()
+        target = PIL.ImageOps.equalize(self.pil_img)
+        pred = Equalize(1.)(self.torch_img)
+        self.compare(target, pred)
 
     def test_solarize(self):
-        raise NotImplemented()
+        mag = 110
+        target = PIL.ImageOps.solarize(self.pil_img, mag)
+        pred = Solarize(mag / 255.)(self.torch_img)
+        self.compare(target, pred)
 
     def test_posterize(self):
-        raise NotImplemented()
+        mag = 4
+        target = PIL.ImageOps.posterize(self.pil_img, mag)
+        pred = Solarize(1.)(self.torch_img)
+        self.compare(target, pred)
 
     def test_sharpness(self):
-        raise NotImplemented()
+        target = PIL.ImageEnhance.Sharpness(self.pil_img).enhance(1.3)
+        pred = Solarize(0.3)(self.torch_img)
+        self.compare(target, pred)
+
+    def test_identity(self):
+        target = self.pil_img
+        pred = Identity(1.)(self.torch_img)
+        self.compare(target, pred)
+
+    def compare(self, pil_img, torch_img):
+        self.assertTrue(np.allclose((np.array(pil_img)/255.).transpose(2, 0, 1),
+                                    torch_img.numpy()))
 
 
 if __name__ == '__main__':
