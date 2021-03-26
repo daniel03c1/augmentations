49752a44f94bb3a0b723d887bf340f10dae394a5
daniel03c1 <daniel03c195@gmail.com>
2021-03-26 11:16:13 +0000

    use wrn from (tensorflow/models/research/autoaugment)
    the network structure is quite different from other codes
    for residual connections, instead of conv2d, avgpool and padding
    were used. Furthermore, additional connections were also add

--- wideresnet.py
+++ wideresnet.py
@@ -2,6 +2,7 @@
 # https://github.com/szagoruyko/wide-residual-networks
 # https://github.com/kakaobrain/fast-autoaugment/blob/master/FastAutoAugment/networks/wideresnet.py
 # https://pytorch.org/vision/0.8/_modules/torchvision/models/resnet.html#wide_resnet50_2
+# https://github.com/tensorflow/models/blob/e356598a5b79a768942168b10d9c1acaa923bdb4/research/autoaugment/wrn.py
 import torch
 import torch.nn as nn
 import torch.nn.init as init
@@ -16,14 +17,20 @@ class WideResNet(nn.Module):
         n = (depth-4) // 6
         widths = [int(v * width) for v in (16, 32, 64)]
 
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1, bias=True)
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
 
         self.layer0 = self.add_layer(
-            WideResNetBlock, 16, widths[0], n, dropout_rate, stride=1)
+            WideResNetBlock, 16, widths[0], n, dropout_rate, stride=1,
+            activate_before_residual=True)
         self.layer1 = self.add_layer(
             WideResNetBlock, widths[0], widths[1], n, dropout_rate, stride=2)
         self.layer2 = self.add_layer(
             WideResNetBlock, widths[1], widths[2], n, dropout_rate, stride=2)
+
+        self.link0 = Linker(16, widths[0], 1, strides=1)
+        self.link1 = Linker(widths[0], widths[1], 2, strides=2)
+        self.link2 = Linker(widths[1], widths[2], 2, strides=2)
+        self.link_final = Linker(16, widths[-1], 4, strides=4)
 
         self.bn = nn.BatchNorm2d(widths[-1])
         self.fc = nn.Linear(widths[-1], n_classes)
@@ -32,8 +39,10 @@ class WideResNet(nn.Module):
 
     def add_layer(self, block, 
                   inplanes, outplanes, 
-                  n_blocks, dropout_rate, stride):
-        layers = [block(inplanes, outplanes, dropout_rate, stride)]
+                  n_blocks, dropout_rate, stride,
+                  activate_before_residual=False):
+        layers = [block(inplanes, outplanes, dropout_rate, stride,
+                        activate_before_residual)]
 
         for _ in range(1, n_blocks):
             layers.append(block(outplanes, outplanes, dropout_rate, 1))
@@ -42,10 +51,13 @@ class WideResNet(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
+        first_x = x
 
-        x = self.layer0(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
+        x = self.layer0(x) + self.link0(x)
+        x = self.layer1(x) + self.link1(x)
+        x = self.layer2(x) + self.link2(x)
+
+        x += self.link_final(first_x)
 
         x = F.relu(self.bn(x))
         x = F.avg_pool2d(x, 8)
@@ -56,28 +68,30 @@ class WideResNet(nn.Module):
 
 
 class WideResNetBlock(nn.Module):  
-    def __init__(self, inplanes, outplanes, dropout_rate, stride):
+    def __init__(self, inplanes, outplanes, dropout_rate, stride,
+                 activate_before_residual=False):
         super(WideResNetBlock, self).__init__()
         self.bn1 = nn.BatchNorm2d(inplanes)
         self.bn2 = nn.BatchNorm2d(outplanes)
         self.dropout = nn.Dropout(p=dropout_rate)
 
-        self.conv1 = nn.Conv2d(
-            inplanes, outplanes, 3, stride=stride, padding=1, bias=True)
-        self.conv2 = nn.Conv2d(
-            outplanes, outplanes, 3, stride=1, padding=1, bias=True)
+        self.conv1 = nn.Conv2d(inplanes, outplanes, 3, stride=stride, padding=1)
+        self.conv2 = nn.Conv2d(outplanes, outplanes, 3, stride=1, padding=1)
 
         if inplanes == outplanes and stride == 1:
             self.link = None
         else:
-            self.link = nn.Conv2d(
-                inplanes, outplanes, 1, stride=stride, bias=True)
+            self.link = Linker(inplanes, outplanes, stride, strides=stride)
+        self.activate_before_residual = activate_before_residual
         
         init_parameters(self.modules())
 
     def forward(self, x):
         out0 = self.bn1(x)
         out0 = F.relu(out0, inplace=True)
+
+        if self.activate_before_residual:
+            x = out0
 
         out = self.conv1(out0)
         out = self.bn2(out)
@@ -89,6 +103,18 @@ class WideResNetBlock(nn.Module):
             return out + self.link(out0)
         else:
             return out + x
+
+
+class Linker(nn.Module):  
+    def __init__(self, inplanes, outplanes, kernel_size, strides):
+        super(Linker, self).__init__()
+        self.avgpool = nn.AvgPool2d(kernel_size, strides)
+        self.pad = (0, 0, 0, 0) + ((outplanes-inplanes)//2,) * 2
+
+    def forward(self, x):
+        x = self.avgpool(x)
+        x = F.pad(x, self.pad, 'constant', 0)
+        return x
 
 
 def init_parameters(modules):
