e4d5787ad109f0452d4273f0e2e0508387eac249
daniel03c1 <daniel03c195@gmail.com>
2021-04-11 02:44:07 +0000

    agents update

--- agents.py
+++ agents.py
@@ -111,3 +111,108 @@ class PPOAgent:
             reward *= gamma
             self.cache(state, action, dist, reward, next_state)
 
+
+class DiscretePPOAgent:
+    def __init__(self, 
+                 net: nn.Module, 
+                 name: str,
+                 mem_maxlen: int,
+                 lr=0.00035,
+                 grad_norm=0.5,
+                 batch_size=1, 
+                 epsilon=0.2,
+                 ent_coef=1e-5,
+                 augmentation=None,
+                 device=None):
+        if device:
+            self.device = device
+        else:
+            self.device = get_default_device()
+        self.net = net.to(self.device)
+        self.name = name
+        self.optimizer = optim.Adam(self.net.parameters(), lr=lr)
+        self.grad_norm = grad_norm
+
+        self.memory = deque(maxlen=mem_maxlen)
+        self.batch_size = batch_size
+        self.epsilon = epsilon
+        self.ent_coef = ent_coef
+        self.augmentation = augmentation
+
+    def act(self, states, rand_prob=0., train=True, **kwargs):
+        if train:
+            self.net.train()
+        else:
+            self.net.eval()
+        return self.net(states, rand_prob, **kwargs)
+
+    def cache(self, state, action, reward, next_state):
+        self.memory.append((state.to(self.device), 
+                            action.to(self.device).detach(),
+                            reward.to(self.device), 
+                            next_state.to(self.device)))
+
+    def cache_batch(self, states, actions, rewards, next_states):
+        for i in range(states.size(0)):
+            self.cache(states[i], actions[i], rewards[i], 
+                       next_states[i])
+
+    def learn(self, n_steps=1):
+        self.net.train()
+
+        for i in range(n_steps):
+            self.optimizer.zero_grad()
+
+            # inputs
+            memory = self.recall()
+            if self.augmentation:
+                memory = self.augmentation(*memory)
+            states, old_actions, rewards, next_states = memory
+
+            actions = self.net(states)
+
+            # TODO: precalculate log_probs of dists
+            ratios = self.net.calculate_log_probs(actions) \
+                   - self.net.calculate_log_probs(old_actions).detach()
+            # for stability
+            ratios = torch.exp(torch.clamp(ratios, -10, 10))
+
+            loss = -torch.min(
+                ratios*rewards,
+                torch.clamp(ratios, 1-self.epsilon, 1+self.epsilon)*rewards)
+            loss -= self.ent_coef * self.net.calculate_entropy(actions)
+
+            torch.mean(loss).backward(retain_graph=True)
+            torch.nn.utils.clip_grad_norm_(self.net.parameters(), 
+                                           self.grad_norm)
+            self.optimizer.step()
+
+        return loss # the last loss
+
+    def recall(self):
+        batch = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, next_states = map(
+            torch.stack, zip(*batch))
+        return states, actions, rewards, next_states
+
+    def clear_memory(self):
+        self.memory.clear()
+
+    def save(self, name=None):
+        if not name:
+            name = self.name
+        if not name.endswith('.pt'):
+            name += '.pt'
+        torch.save(self.net.state_dict(), name)
+
+    def decode_policy(self, *args, **kwargs):
+        return self.net.decode_policy(*args, **kwargs)
+
+    def apply_gamma(self, gamma):
+        # gamma refers deprecation rate
+        # it multiplies gamma to every reward in the queue
+        for i in range(len(self.memory)):
+            state, action, reward, next_state = self.memory.popleft()
+            reward *= gamma
+            self.cache(state, action, reward, next_state)
+
